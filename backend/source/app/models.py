8c9c9a8b424182e9e42f7b1bd194023144f6762c
nobakit <nobuaki.ito.0@gmail.com>
2023-02-03 00:12:23 +0900

    modify model

--- backend/source/app/models.py
+++ backend/source/app/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 class KanjiSingle(models.Model):
-    # NOTE: pkを明示的に指定しないとidがpkとして自動で設定されるらしい
-    kanji = models.CharField(max_length=5)
+    kanji = models.CharField(max_length=5, primary_key=True)
+    # tag_1 = models.SmallIntegerField(max_length=4)
+    # tag_2 = models.SmallIntegerField(max_length=4)
+    # tag_3 = models.SmallIntegerField(max_length=4)
 
 class KanjiIdiom(models.Model):
-    idiom = models.CharField(max_length=10)
+    idiom = models.CharField(max_length=10, primary_key=True)
+    # tag_1 = models.SmallIntegerField(max_length=4)
+    # tag_2 = models.SmallIntegerField(max_length=4)
+    # tag_3 = models.SmallIntegerField(max_length=4)
+
+class Tag(models.Model):
+    # NOTE: pkを明示的に指定しないとidがpkとして自動で設定されるらしい
+    tag_name = models.CharField(max_length=10)
