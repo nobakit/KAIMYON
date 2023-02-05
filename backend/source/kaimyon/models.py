6272078416ce93913b045b0e15f4277e5ae29329
nobakit <nobuaki.ito.0@gmail.com>
2023-02-06 00:10:38 +0900

    [change] カラムにユニーク制約付与

--- backend/source/kaimyon/models.py
+++ backend/source/kaimyon/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# NOTE: pkを明示的に指定しないとidがpkとして自動で設定されるらしい
 
 class KanjiSingle(models.Model):
-    kanji = models.CharField(max_length=5, primary_key=True)
+    kanji = models.CharField(max_length=5, unique=True)
     # tag_1 = models.SmallIntegerField(max_length=4)
     # tag_2 = models.SmallIntegerField(max_length=4)
     # tag_3 = models.SmallIntegerField(max_length=4)
 
 class KanjiIdiom(models.Model):
-    idiom = models.CharField(max_length=10, primary_key=True)
+    idiom = models.CharField(max_length=10, unique=True)
     # tag_1 = models.SmallIntegerField(max_length=4)
     # tag_2 = models.SmallIntegerField(max_length=4)
     # tag_3 = models.SmallIntegerField(max_length=4)
 
 class Tag(models.Model):
-    # NOTE: pkを明示的に指定しないとidがpkとして自動で設定されるらしい
-    tag_name = models.CharField(max_length=30)
+    tag_name = models.CharField(max_length=30, unique=True)
