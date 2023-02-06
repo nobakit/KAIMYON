36decf5961615a3ceb564da1d61bf24a171e42b1
nobakit <nobuaki.ito.0@gmail.com>
2023-02-07 01:33:53 +0900

    [change] #2 テーブル定義変更

--- backend/source/kaimyon/models.py
+++ backend/source/kaimyon/models.py
@@ -3,17 +3,50 @@ from django.db import models
 # Create your models here.
 # NOTE: pkを明示的に指定しないとidがpkとして自動で設定されるらしい
 
+
 class KanjiSingle(models.Model):
-    kanji = models.CharField(max_length=5, unique=True)
-    # tag_1 = models.SmallIntegerField(max_length=4, null=False)
-    # tag_2 = models.SmallIntegerField(max_length=4, null=False)
-    # tag_3 = models.SmallIntegerField(max_length=4, null=False)
+    kanji = models.CharField(max_length=1, primary_key=True)
+
+    def __str__(self):
+        return self.kanji
+
 
 class KanjiIdiom(models.Model):
-    idiom = models.CharField(max_length=10, unique=True)
-    # tag_1 = models.SmallIntegerField(max_length=4, null=False)
-    # tag_2 = models.SmallIntegerField(max_length=4, null=False)
-    # tag_3 = models.SmallIntegerField(max_length=4, null=False)
+    idiom = models.CharField(max_length=10, primary_key=True)
+
+    def __str__(self):
+        return self.idiom
+
 
 class Tag(models.Model):
+    tag_id = models.AutoField(primary_key=True)
     tag_name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return f"{self.tag_id}: {self.tag_name}"
+
+
+class KanjiTagRelation(models.Model):
+    kanji = models.ForeignKey(KanjiSingle, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.id}: {self.kanji}, {self.tag}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["kanji", "tag"], name="unique_kanji_tag")
+        ]
+
+
+class IdiomTagRelation(models.Model):
+    idiom = models.ForeignKey(KanjiIdiom, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.id}: {self.idiom}, {self.tag}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["idiom", "tag"], name="unique_idiom_tag")
+        ]
