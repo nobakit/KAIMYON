a4088bbec12881f4b30b7911dbea5f158b000b1b
nobakit <nobuaki.ito.0@gmail.com>
2023-02-13 00:38:32 +0900

    add: #5 adminからのデータ投入用設定追加

--- backend/source/kaimyon/admin.py
+++ backend/source/kaimyon/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import KanjiSingle, KanjiIdiom, Tag, KanjiTagRelation, IdiomTagRelation
 
 # Register your models here.
+
+admin.site.register(KanjiSingle)
+admin.site.register(KanjiIdiom)
+admin.site.register(Tag)
+
+admin.site.register(KanjiTagRelation)
+admin.site.register(IdiomTagRelation)
